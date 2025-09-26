e1efb197ce7b9d318f3c129c74381dd9fd4997ed
KAYKY-05 <marquesinek@gmail.com>
2025-09-26 17:09:44 -0300

    primeiro commit

---
+++ POKEMON.js
@@ -0,0 +1,22 @@
+let cadastroPokemon = [
+["Poochyena ","lv2 ","M ","PV 13/13 "],
+["Zigzagoon ","lv2 ","F ","PV 13/13 "],
+["Dragonite ","lv5 ","M ","PV 25/25 "],
+["Dragonite ","lv5 ","F ","PV 24/24 "],
+["Dragonite ","lv5 ","F ","PV 24/24 "],
+["Poochyena ","lv3 ","F ","PV 15/15 "],
+["Hurmple ","lv2 ","M ","PV 7/14 "]]
+
+const Mensagem = "cadastrado com sucesso"
+
+console.log( cadastroPokemon[0][0] + Mensagem  )
+
+console.log( cadastroPokemon[1][0] + Mensagem  )
+
+console.log( cadastroPokemon[2][0] + Mensagem  )
+
+console.log( cadastroPokemon[3][0] + Mensagem  )
+
+console.log( cadastroPokemon[4][0] + Mensagem  )
+
+console.log( cadastroPokemon[5][0] + Mensagem  )
